3382daf66ec1b8098d0c5e50aa77cc0636c5686c
Bogdan Malcev <bogdan-malcev@yandex.ru>
2023-04-05 20:49:20 +0700

    migrations

---
+++ GitlabMonitor/Migrations/20230405134534_init.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GitlabMonitor.Migrations
+{
+    /// <inheritdoc />
+    public partial class init : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Reviewers",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    UserId = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Reviewers", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "AssignedMergeRequests",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    ReviewerId = table.Column<int>(type: "INTEGER", nullable: false),
+                    Title = table.Column<string>(type: "TEXT", nullable: false),
+                    References = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AssignedMergeRequests", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_AssignedMergeRequests_Reviewers_ReviewerId",
+                        column: x => x.ReviewerId,
+                        principalTable: "Reviewers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AssignedMergeRequests_ReviewerId",
+                table: "AssignedMergeRequests",
+                column: "ReviewerId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "AssignedMergeRequests");
+
+            migrationBuilder.DropTable(
+                name: "Reviewers");
+        }
+    }
+}
